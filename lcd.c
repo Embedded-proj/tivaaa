b6178896e9b28740432872d502f309cc3a81452e
Shimaagamal21 <85262722+Shimaagamal21@users.noreply.github.com>
2021-06-05 16:49:29 +0200

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,15 +1,41 @@
+//delay function in milli 
+void dmilli(int n){
+int i,j;
+for(i=0;i<n;i++)
+for(j=0;j<3180;j++)
+{}
+}
+
+// delay function in micro
+void dmicro(int n){
+int i,j;
+for(i=0;i<n;i++)
+for(j=0;j<3;j++)
+{}
+}
+
 void LCD_Cmd( char command)
 {
-GPIO_PORTA_DATA_R |= 0x00; 
-GPIO_PORTB_DATA_R =command;
-GPIO_PORTA_DATA_R |=0x80; 
-GPIO_PORTA_DATA_R |=0x00;
+
+	GPIO_PORTB_DATA_R = command;
+	GPIO_PORTD_DATA_R &= ~0x01; 
+	GPIO_PORTD_DATA_R &= ~0x02;
+	dmilli(1);
+	GPIO_PORTD_DATA_R |=0x04; 
+	dmicro(1);
+	GPIO_PORTD_DATA_R &= ~0x04;
+	dmicro(5);
+
 }
 
 void LCD_Data( char data) 
 { 
-GPIO_PORTA_DATA_R |=0x20; 
-GPIO_PORTB_DATA_R =data;
-GPIO_PORTA_DATA_R |= 0x80;
-GPIO_PORTA_DATA_R  =0x00;
+GPIO_PORTB_DATA_R = data;
+GPIO_PORTD_DATA_R |= 0x01; 
+GPIO_PORTD_DATA_R &= ~ 0x02; 
+GPIO_PORTD_DATA_R |= 0x04; 
+dmicro(1);
+GPIO_PORTD_DATA_R &= ~0x04; 
+dmicro(1);
 }
+
