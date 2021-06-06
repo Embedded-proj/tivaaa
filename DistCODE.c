48cb2378f5b4a6d8804361b15bf1d8673563095c
bassanttsayed <85302147+bassanttsayed@users.noreply.github.com>
2021-06-06 05:49:25 +0200

    Update DistCODE.c

--- DistCODE.c
+++ DistCODE.c
@@ -1,4 +1,16 @@
-
+//////////////UART initialization//////////
+	SYSCTL_RCGCGPIO_R |= 0x10;	//enable port E
+	while( (SYSCTL_PRGPIO_R & 0x10) == 0){}
+	SYSCTL_RCGCUART_R  |= 0x20;  //enable UART5 clock
+	UART5_CTL_R |=	0x00000000;
+	GPIO_PORTE_DEN_R |= 0x30;	//E4,E5 digital
+	GPIO_PORTE_AMSEL_R &= ~ 0x30;	// disable analog
+	GPIO_PORTE_AFSEL_R |=  0x30;	
+	GPIO_PORTE_PCTL_R |=  0x00110000;	
+	UART5_IBRD_R  = 104;
+	UART5_FBRD_R  = 11;	
+	UART5_LCRH_R  = 0x70;
+        UART5_CTL_R  = 0x201;	//reciever only	
 
 const double PI = 3.1415926;
 static double calcDist( double x1, double y1, double x2, double y2)
