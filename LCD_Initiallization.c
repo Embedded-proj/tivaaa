8fe95ca3482df4243cfe4f68c675b4668980d48b
mennahusseiin <85253970+mennahusseiin@users.noreply.github.com>
2021-06-06 06:03:47 +0200

    Update LCD_Initiallization.c

--- LCD_Initiallization.c
+++ LCD_Initiallization.c
@@ -1,21 +1,20 @@
-void LCD_init_ports(void){
+void ports_initiallization(void){
 
-//PORTD_Initiallization
-//We used PORTD for control : PD0=Rs , PD1=R/W , PD2=E
-	SYSCTL_RCGCGPIO_R |= 0x08;
-//while ((SYSCTL_PRGPIO_R & 0x08) == 0){}
-	GPIO_PORTD_LOCK_R = 0x4C4F434B;
-	GPIO_PORTD_CR_R = 0x07;
-	GPIO_PORTD_DIR_R |=0x07;
-	GPIO_PORTD_DEN_R|=0x07;
-	GPIO_PORTD_AMSEL_R &= ~ 0x07;	
-	GPIO_PORTD_AFSEL_R &= ~ 0x07;	
-	GPIO_PORTD_PCTL_R &= ~ 0x00000FFF;
+//PORTA_Initiallization
+//We used PORTA for control : PD0=Rs , PD1=R/W , PD2=E
+	SYSCTL_RCGCGPIO_R |= 0x01;
+	while ((SYSCTL_PRGPIO_R & 0x01) == 0){}
+	GPIO_PORTA_CR_R = 0xE0;
+	GPIO_PORTA_DIR_R |=0xE0;
+	GPIO_PORTA_DEN_R|=0xE0;
+	GPIO_PORTA_AMSEL_R &= ~0xE0;	
+	GPIO_PORTA_AFSEL_R &= ~ 0xE0;	
+	GPIO_PORTA_PCTL_R &= ~ 0xFFF00000;
 		
 //PORTB_Initiallization
 //We used PORTB for data from PB0 to PB7
 	SYSCTL_RCGCGPIO_R |= 0x02; 
-//while ((SYSCTL_PRGPIO_R & 0x02) == 0){}
+	while ((SYSCTL_PRGPIO_R & 0x02) == 0){}
 	GPIO_PORTB_CR_R = 0xFF;
 	GPIO_PORTB_DIR_R |=0xFF; 
 	GPIO_PORTB_DEN_R |=0xFF;
@@ -36,4 +35,13 @@ void LCD_init(void){
 	dmicro(50);
 	LCD_Cmd(0x01); 
 	dmilli(5);
+}
+
+//Converting the distance into a chararcter
+char x[3];
+void convert_to_char(int a )
+{
+	x[0]= convert((a/100)%10) ;
+	x[1]=	convert( (a/10)%10 ) ;
+	x[2]=convert(a%10);
 }
